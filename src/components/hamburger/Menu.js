9b8a722dcc8792fa3860ec63204db9546a7539d1
Deevoid <dev.4.pandey@gmail.com>
2020-06-29 20:25:19 +0530

    hello world

--- src/components/hamburger/Menu.js
+++ src/components/hamburger/Menu.js
@@ -1,6 +1,7 @@
 import * as React from "react";
 import { motion } from "framer-motion";
 import ListMenu from "./ListMenu";
+import Navigation from "../navigation";
 
 const variants = {
   open: {
@@ -13,9 +14,10 @@ const variants = {
 
 export const Menu = () => (
   <motion.ul variants={variants}>
-    {itemIds.map((i) => (
+    <Navigation />
+    {/* {itemIds.map((i) => (
       <ListMenu i={i} key={i} />
-    ))}
+    ))} */}
   </motion.ul>
 );
 const itemIds = [0, 1, 2, 3, 4];
--- src/components/navigation.js
+++ src/components/navigation.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { Link } from "gatsby";
 
-export default (props) => (
-  <nav className="navigation">
-    <Link to="/blog">Blog</Link>
-    <Link to="/symptoms">Symptoms</Link>
-    <Link to="/safety">Safety</Link>
-    <Link to="/helpline">Helpline</Link>
-  </nav>
-);
+export default (props) => {
+  return (
+    <nav className="navigation">
+      <Link to="/blog">Blog</Link>
+      <Link to="/symptoms">Symptoms</Link>
+      <Link to="/safety">Safety</Link>
+      <Link to="/helpline">Helpline</Link>
+    </nav>
+  );
+};
